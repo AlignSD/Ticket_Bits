847ee5a6f38d3b7db85f9749e365a1f19517a361
TJ Hutchinson <tjhutch92@gmail.com>
2021-03-01 16:42:37 -0800

    Intial Blockchain code. Account still coming back undefined. Needs Fixing. Be sure to download metadesk chrome extentions

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Web3 from 'web3';
 import "./App.css";
 
 class App extends Component {
+
+componentWillMount() {
+  this.loadBlockchainData();
+}
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const network = await web3.eth.net.getNetworkType();
+    console.log("network: ", network);
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] });
+    console.log("account", accounts[0]);
+  }
+
+
+  constructor(props) {
+    super(props);
+    this.state = { account : "" }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello, World!</h1>
+        <p>Your Account: {this.state.account}</p>
       </div>
     );
   }
