422eac5da4cbc24c171246e5f02ff23185a2d528
TJ Hutchinson <tjhutch92@gmail.com>
2021-03-03 18:51:12 -0800

    Ticket Blockchain Marketplace App functional, but needs frontend

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+
+  render() {
+    return (
+        <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ticket Bits
+          </a>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className="text-white"><span id="account">{this.props.account}</span></small>
+            </li>
+          </ul>
+        </nav>
+        </div>
+    );
+  }
+}
+
+export default Navbar;
