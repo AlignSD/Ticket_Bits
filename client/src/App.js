1f68be68a2974f596eb4ef743800cff4a33e51b6
WebmasterShane <shanelwebber@gmail.com>
2021-03-05 02:20:47 -0800

    working authentication and routing

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Login from "../src/pages/login"
+
+import './App.css';
+// import LoginButton from "./components/LoginButton"
+// import LogoutButton from './components/LogoutButton'
+// import TicketMarketPlace from '../src/components/TicketMarketPlace'
+// import Button from '@material-ui/core/Button'
+// import { useAuth0 } from "@auth0/auth0-react";
+
+
+
+function App() {
+  return (
+    <div>      
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
+
+  
+  
