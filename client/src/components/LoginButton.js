047b0479a4af48f44eb7eb36b995ec74266240f0
Larry Gamboa <techmedia.lg@gmail.com>
2021-03-18 22:44:33 -0700

    update top navigation

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -1,14 +1,65 @@
 import React from "react";
+import { Button, IconButton, makeStyles, Toolbar } from "@material-ui/core";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import { useAuth0 } from "@auth0/auth0-react";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  iconButton: {
+    marginLeft: 20,
+    paddingBottom: 6,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+  },
+}));
+
 const LoginButton = () => {
+
   const { loginWithRedirect } = useAuth0();
+
+  const classes = useStyles();
+
   return (
-    <button
-      className="btn btn-primary btn-block"
-      onClick={() => loginWithRedirect()}>
-      Log In
-    </button>
+    <div className={classes.root}>
+      
+        <Toolbar>
+          
+          <Button 
+            color="inherit"
+            onClick={() => loginWithRedirect()}
+            variant="contained"
+            color="primary"
+          >
+            Login
+          </Button>
+          
+          <IconButton edge="start" className={classes.iconButton}>
+            <a href="/" style={{ color: `white` }}>
+              <AccountCircleIcon fontSize="medium" />
+              <span style={{ fontSize: 14 }}>darthvader</span>
+            </a>
+          </IconButton>
+
+          <IconButton edge="start" className={classes.iconButton}>
+            <a href="/" style={{ color: `white` }}>
+              <ShoppingCartIcon fontSize="medium" />
+              <span style={{ fontSize: 14 }}>CHECKOUT</span>
+            </a>
+          </IconButton>
+
+        </Toolbar>
+      
+    </div>
   );
 };
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,7 +7,7 @@ import AuthNav from "./auth-nav";
 function NavBar(props){
   return (
     <div className="nav-container mb-3">
-      <nav className="navbar navbar-expand-md navbar-light bg-light">
+      <nav className="navbar navbar-expand-md navbar-light bg-primary">
         <div className="container">
           <div className="navbar-brand logo" />
           <MainNav
