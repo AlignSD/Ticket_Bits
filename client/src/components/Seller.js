48331df20b298b7cbfae5f918341b93986e8e726
TJ Hutchinson <tjhutch92@gmail.com>
2021-03-22 00:10:35 -0700

    Added content working with contracts, Images commented out needs work

--- client/src/components/Seller.js
+++ client/src/components/Seller.js
@@ -1,7 +1,8 @@
-import React, {  useState } from "react";
+import React, {  useState, useContext } from "react";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles} from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import {TicketsContext} from '../utils/TicketsContext'
 
 // Event host sells tickets thru Seller component
 // could be converted into a functional component if bored
@@ -33,10 +34,47 @@ const useStyles = makeStyles({
   },
 });
 function Seller(props) {
+
+//Declare IPFS
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) // leaving out the arguments will default to these values
+
+  let {setCaptureFile} = useContext(TicketsContext)
+
   const [input, setInput] = useState({
-    name: "",
-    prce: ''
+    imgHash: '',
+    name: '',
+    prce: '',
+    startDate: '',
+    location: '',
+    description: ''
   })
+
+  // function convertFile (captureFile) {
+  //   // console.log(captureFile)
+  //   const file = new Blob([captureFile], {type: '.jpg, .jpeg, .png, .bmp, .gif'})
+  //   const reader = new window.FileReader()
+  //   reader.readAsArrayBuffer(file)
+
+  //   reader.onloadend = () => {
+  //     setInput({ input, msgHash: Buffer(reader.result) })
+  //     console.log('buffer', input.msgHash)
+  //   } 
+
+  //       console.log("Submitting file to ipfs...")
+
+  //       //adding file to the IPFS
+  //       ipfs.add(input.msgHash, (error, result) => {
+  //         console.log('Ipfs result', result)
+  //         if (error) {
+  //           console.error(error)
+  //           return
+  //         }
+
+  //       })
+  //       return input.msgHash
+  // }
+
   const classes = useStyles();
 
   return (
@@ -48,14 +86,58 @@ function Seller(props) {
               <h1 style={{ color: "black" }}>Add Ticket</h1>
               <form
                 onSubmit={(event) => {
+                  
                   event.preventDefault();
+                  // convertFile();                 
+                  // console.log(event)
+                  // console.log(captureFile)
+                  // let blob = new Blob([captureFile], {type: '.jpg, .jpeg, .png, .bmp, .gif'})
+                  // console.log(blob)
+
+
+                  // const reader = new FileReader()
+                  // reader.readAsArrayBuffer(blob)
+                  
+                  // reader.onloadend = () => {
+
+                  //   console.log(reader.result)
+                  //   setInput({...input, imgHash: Buffer.from(reader.result) })
+                  //   console.log(input.imgHash)
+                  //   console.log('buffer', input.imgHash)
+                  // }
+
+                  //     ipfs.add(input.imgHash, (error, result) => {
+                  //       console.log('Ipfs result', result)
+                  //       if(error) {
+                  //         console.error(error)
+                  //         return
+                  //       }
+
+
+                  //       })
+                  // const imgHash = input.imgHash;
                   const name = input.name;
                   const price = window.web3.utils.toWei(
                     input.price,
                     "Ether");
-                  props.createTicket(name, price);
+                  const startDate = input.startDate;
+                  const location = input.location;
+                  const description = input.description;
+                  console.log( name, price, startDate, location, description)
+                  props.createTicket( name, price, startDate, location, description);
                 }}
               >
+                {/* <div className="form-group mr-sm-2">
+                  <input 
+                    style={{ widith: "30%" }}
+                    id="ticketImgHash"
+                    type='file' accept=".jpg, .jpeg, .png, .bmp, .gif"
+                    className="form"
+                    placeholder="Image"
+                    required
+                    onChange={(e) => setCaptureFile({captureFile: e.target.value})}
+                  />
+                </div> */}
                 <div className="form-group mr-sm-2">
                   <input
                     style={{ widith: "30%" }}
@@ -77,6 +159,39 @@ function Seller(props) {
                     required
                     onChange={(e) => setInput({...input, price: e.target.value})}
                   />
+                </div>
+                <div className="form-group mr-sm-2">
+                  <input
+                    style={{ widith: "30%" }}
+                    id="ticketStartDate"
+                    type="text"
+                    className="form"
+                    placeholder="Start Date"
+                    required
+                    onChange={(e) => setInput({...input, startDate: e.target.value})}
+                  />
+                </div>
+                <div className="form-group mr-sm-2">
+                  <input
+                    style={{ widith: "30%" }}
+                    id="ticketLoaction"
+                    type="text"
+                    className="form"
+                    placeholder="Loaction"
+                    required
+                    onChange={(e) => setInput({...input, location: e.target.value})}
+                  />
+                  <div className="form-group mr-sm-2">
+                  <input
+                    style={{ widith: "30%" }}
+                    id="ticketDescription"
+                    type="text"
+                    className="form"
+                    placeholder="Description"
+                    required
+                    onChange={(e) => setInput({...input, description: e.target.value})}
+                  />
+                </div>
                 </div>
                 <Button type="submit" className={classes.btnMargin}>
                   Add Ticket
