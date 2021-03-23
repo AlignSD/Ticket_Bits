ca49bacedf3a64adb56e64515b8bd7ada0aa8c04
WebmasterShane <shanelwebber@gmail.com>
2021-03-22 23:38:43 -0700

    shane

---
+++ client/build/precache-manifest.55e472ad1dda592a558a21f8363d8f47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "71dafc7573dc1eb0c980",
+    "url": "/static/css/main.a25fed77.chunk.css"
+  },
+  {
+    "revision": "71dafc7573dc1eb0c980",
+    "url": "/static/js/main.71dafc75.chunk.js"
+  },
+  {
+    "revision": "0e8d1c917f49f8642ff1",
+    "url": "/static/css/1.55c3484d.chunk.css"
+  },
+  {
+    "revision": "0e8d1c917f49f8642ff1",
+    "url": "/static/js/1.0e8d1c91.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f94fc1ea80db4df05a704a2299e0577",
+    "url": "/static/media/CrypticLogo.8f94fc1e.png"
+  },
+  {
+    "revision": "57d7ce13093381acee3d40c5a512c770",
+    "url": "/static/media/dogica.57d7ce13.ttf"
+  },
+  {
+    "revision": "2cb48bb25884c9fa3ac18d731db7ec87",
+    "url": "/index.html"
+  }
+];
