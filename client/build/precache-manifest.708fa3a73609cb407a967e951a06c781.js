f27b7d333e97945d7992319321f4fc308969d639
Evan Alioto <EvanJAlioto@gmail.com>
2021-03-22 23:52:20 -0700

    will it work? tune in next week

---
+++ client/build/precache-manifest.708fa3a73609cb407a967e951a06c781.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e71974237b5b18b1a21f",
+    "url": "/static/css/main.e8498a10.chunk.css"
+  },
+  {
+    "revision": "e71974237b5b18b1a21f",
+    "url": "/static/js/main.e7197423.chunk.js"
+  },
+  {
+    "revision": "20664780e0fa5b57d09b",
+    "url": "/static/css/1.5401f530.chunk.css"
+  },
+  {
+    "revision": "20664780e0fa5b57d09b",
+    "url": "/static/js/1.20664780.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f94fc1ea80db4df05a704a2299e0577",
+    "url": "/static/media/CrypticLogo.8f94fc1e.png"
+  },
+  {
+    "revision": "57d7ce13093381acee3d40c5a512c770",
+    "url": "/static/media/dogica.57d7ce13.ttf"
+  },
+  {
+    "revision": "564b46a792a71b151dcc6fbf77736f5a",
+    "url": "/index.html"
+  }
+];
