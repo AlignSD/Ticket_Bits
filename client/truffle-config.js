c9c689871d9ac7f39f0a2754833b1fc687a6bc20
Evan Alioto <EvanJAlioto@gmail.com>
2021-03-22 22:30:52 -0700

    heroku deployment

--- client/truffle-config.js
+++ client/truffle-config.js
@@ -1,5 +1,9 @@
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const privateKeys = process.env.PRIVATE_KEYS || "";
+
 
 module.exports = {
   networks: {
@@ -8,6 +12,18 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(
+          privateKeys.split(','),
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
+          )
+        },
+    
+        gas: 4000000, 
+        network_id: 3
+      }
+  
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
